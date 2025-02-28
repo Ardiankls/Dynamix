c3f5eea1e5aac56b796d0483488564ee96b29dc5
ardian <ardiankurniawan7@gmail.com>
2025-02-28 07:28:51 +0700

    Update Welcome.js

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -9,7 +9,7 @@ const Welcome = () => {
       <h1>Welcome!</h1>
       <p>
         You have <strong>3 seconds</strong> for each shot – no retakes! <br />
-        This photobooth captures <strong>4 pictures</strong> in a row, so strike your best pose and have fun!
+        This photobooth captures <strong>3 pictures</strong> in a row, so strike your best pose and have fun!
       </p>
       <p>
         After the session, <span style={{ color: "pink" }}></span> download your digital copy and share the fun!
